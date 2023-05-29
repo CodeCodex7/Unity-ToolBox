4a12dc33e7673dd57ca3fb0860fe5e9dbbe2ce0b
Oliver Thauerer <Oliverthauerer1@gmail.com>
2023-05-29 13:06:26 +0100

    *Converted Services to static class *Added namespace

--- Assets/Scripts/service/Services.cs
+++ Assets/Scripts/service/Services.cs
@@ -1,61 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using System;
-
-public class Services : MonoBehaviour
+namespace O_UnityToolbox
 {
-
-    public Dictionary<Type, object> services = new Dictionary<Type, object>();
-
-    public static Services Instance;
-
-    public void Awake()
-    {
-        Instance = this;
-    }
-    
     /// <summary>
-    /// Try to get Service 
+    /// Basic Service Locator Pattern
     /// </summary>
-    /// <typeparam name="T"> Generic type </typeparam>
-    /// <returns>Service of type T </returns>
-    public T Resovle<T>() where T : class
+    public static class Services
     {
-        Type typeParameterType = typeof(T);
 
-        if (services.ContainsKey(typeParameterType))
+        public static Dictionary<Type, object> services = new Dictionary<Type, object>();
+
+        /// <summary>
+        /// Try to get Service 
+        /// </summary>
+        /// <typeparam name="T"> Generic type </typeparam>
+        /// <returns>Service of type T </returns>
+        public static T Resovle<T>() where T : class
         {
-            return (T)services[typeParameterType];
+            Type typeParameterType = typeof(T);
+
+            if (services.ContainsKey(typeParameterType))
+            {
+                return (T)services[typeParameterType];
+            }
+
+            Debug.LogError(string.Format("Cant Resovle service of type {0}", typeParameterType));
+            return null;
         }
 
-        Debug.LogError(string.Format("Cant Resovle service of type {0}", typeParameterType));
-        return null;
-    }
-
-    /// <summary>
-    /// Registar Service
-    /// </summary>
-    /// <param name="o"> Object to Registar </param>
-    public void Registar(object o)
-    {
-        services.Add(o.GetType(), o);
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="ObjectType"></param>
-    public void DeRegistar(Type ObjectType)
-    {
-        if(services.ContainsKey(ObjectType))
+        /// <summary>
+        /// Registar Service
+        /// </summary>
+        /// <param name="o"> Object to Registar </param>
+        public static void Registar(object o)
         {
-            services.Remove(ObjectType);
+            services.Add(o.GetType(), o);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ObjectType"></param>
+        public static void DeRegistar(Type ObjectType)
+        {
+            if (services.ContainsKey(ObjectType))
+            {
+                services.Remove(ObjectType);
+            }
         }
     }
-
-
-
-
 }
