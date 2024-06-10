076a6235df9672c6a71e82de7157308dbc507ba8
Oliver Thauerer <oliver.thauerer@metaverse.co.uk>
2024-06-10 17:01:34 +0100

    *WIP

--- Assets/Scripts/GenericStateEngine/GenericStateEngine.cs
+++ Assets/Scripts/GenericStateEngine/GenericStateEngine.cs
@@ -4,42 +4,87 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Linq;
+using System.Text;
 
 //Oliver T, Generic State Engine based service to run State Machines
-public enum StateChange {Next,Previouse,Close}
-public enum StateMachinceStates { Enabled,Disabled}
+
+public enum StateChange { Next, Previous, Close }
+public enum StateMachinceStates { Enabled, Disabled }
+
 public class GenericStateEngine : MonoService<GenericStateEngine>
 {
+    #region Public Varibles
+    public List<MonoStateMachine> LoadedMachines = new List<MonoStateMachine>();  // Could make a Dict because Uniqure ID
+    #endregion
 
-    public List<StateMachine> LoadedMachines = new List<StateMachine>();
+    #region Private Varibles
     public Action NextFrame;
+    #endregion
 
+    #region Unity Functions
     private void Awake()
     {
         RegisterService();
+    }
+
+    void Update()
+    {
+        if (NextFrame != null)
+        {
+            NextFrame.Invoke();
+            NextFrame -= NextFrame;
+        }
+        
+        foreach (MonoStateMachine m in LoadedMachines)
+        {
+            if (m.InteralState == StateMachinceStates.Enabled)
+            {
+                try
+                {
+                    foreach (State Target in m.ActiveStates.ToList())
+                    {
+
+                        Target.Tick();
+
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogException(ex);
+                }
+            }
+        }
     }
 
     private void OnDestroy()
     {
         UnregisterService();
     }
-    public void StartMachine()
+
+    #endregion
+
+    #region State Engine Functions
+    /// <summary>
+    /// Load a StateMachine
+    /// </summary>
+    public void LoadStateMachine()
     {
-        foreach(StateMachine m in LoadedMachines)
+        foreach (MonoStateMachine m in LoadedMachines)
         {
             m.InteralState = StateMachinceStates.Enabled;
-           foreach(State TargetState in m.ActiveStates)
+            foreach (State TargetState in m.ActiveStates)
             {
                 TargetState.In();
             }
-            
+
         }
     }
-    public void StartMachine(StateMachine SM)
+    public void LoadStateMachine(MonoStateMachine SM)
     {
         LoadedMachines.Add(SM);
 
-        foreach (StateMachine m in LoadedMachines)
+        foreach (MonoStateMachine m in LoadedMachines)
         {
             if (m.InteralState == StateMachinceStates.Disabled)
             {
@@ -52,25 +97,13 @@ public class GenericStateEngine : MonoService<GenericStateEngine>
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Unload a State Machine via it GUID id
+    /// </summary>
+    /// <param name="ID">ID of the StateMachine</param>
+    public void UnloadStateMachine(Guid ID)
     {
-        if (NextFrame != null)
-        {
-            NextFrame.Invoke();
-            NextFrame -= NextFrame;
-        }
-
-        foreach (StateMachine m in LoadedMachines)
-        {
-            if (m.InteralState == StateMachinceStates.Enabled)
-            {
-                foreach (State Target in m.ActiveStates.ToList())
-                {
-                    Target.Tick();
-                }
-            }
-        }
+        LoadedMachines.Remove(FindMachine(ID));
     }
 
     /// <summary>
@@ -78,33 +111,49 @@ public class GenericStateEngine : MonoService<GenericStateEngine>
     /// </summary>
     /// <param name="ID"></param>
     /// <param name="ChangeMode"></param>
-    public void ChangeState(Guid ID,StateChange ChangeMode)
+    public void ChangeState(Guid ID, StateChange ChangeMode, string Name)
     {
-        StateMachine Machine = FindMachine(ID);
+        MonoStateMachine Machine = FindMachine(ID);
 
         switch (ChangeMode)
         {
             case StateChange.Next:
-                var StateList = Machine.ActiveStates;
                 foreach (State Target in Machine.ActiveStates.ToList())
                 {
-                    Target.Out(); //Conduct out()
-                 
-                    Target.PreviousState = Machine.ActiveState; //Set Previouse State 
-                    Machine.ActiveState = Target.NextStates.FirstOrDefault();
-                    NextFrame += Machine.ActiveState.In;
+                    if (Target.Name == Name)
+                    {
+                        Target.Out(); //Conduct out()  
+                        Target.PreviousState = Machine.ActiveState; //Set Previouse State                        
+                        Machine.ActiveStates.Remove(Target);
+
+                        Machine.ActiveStates.Add(Target.NextStates.FirstOrDefault());
+                        NextFrame += Machine.ActiveState.In;
+                    }
                 }
 
                 break;
-            case StateChange.Previouse:
-                Machine.ActiveState.Out();
-                Machine.ActiveState = Machine.ActiveState.PreviousState;
-                NextFrame += Machine.ActiveState.In;
+            //Still needs work may not work correctly
+            case StateChange.Previous:
+                foreach (State Target in Machine.ActiveStates.ToList())
+                {
+                    if (Target.Name == Name)
+                    {
+                        Target.Out();                      
+                        Machine.ActiveStates.Remove(Target);
+                        Machine.ActiveStates.Add(Target.PreviousState);
+                        NextFrame += Machine.ActiveState.In;
+                    }
+                }
                 break;
+            case StateChange.Close:
+                {
+                    ExitState(ID, Name, false);
+                }
+                break;
+                        
             default:
                 break;
         }
-
     }
 
     /// <summary>
@@ -113,17 +162,29 @@ public class GenericStateEngine : MonoService<GenericStateEngine>
     /// <param name="ID"></param>
     /// <param name="ChangeMode"></param>
     /// <param name="StateName"></param>
-    public void ChangeState(Guid ID,string StateName)
+    public void ChangeState(Guid ID, string StateName,string Name)
     {
-        var Machine = (from loadedMachine in LoadedMachines
-                       where loadedMachine.MachineId == ID
-                       select loadedMachine).Single();
+        var Machine = FindMachine(ID);
 
-        Machine.ActiveState.Out();
-        Machine.ActiveState.PreviousState = Machine.ActiveState;
-        Machine.ActiveState = FindState(StateName);
-        NextFrame += Machine.ActiveState.In;
 
+        //Machine.ActiveState.Out();
+        //Machine.ActiveState.PreviousState = Machine.ActiveState;
+        //Machine.ActiveState = FindState(StateName);
+        //NextFrame += Machine.ActiveState.In;
+        var JumpState = FindState(StateName);
+
+        foreach (State Target in Machine.ActiveStates.ToList())
+        {
+            if (Target.Name == Name)
+            {
+                Target.Out(); 
+                Target.PreviousState = Machine.ActiveState;                      
+                Machine.ActiveStates.Remove(Target);
+
+                Machine.ActiveStates.Add(JumpState);
+                NextFrame += JumpState.In;
+            }
+        }
 
 
         State FindState(string stateName)
@@ -164,24 +225,26 @@ public class GenericStateEngine : MonoService<GenericStateEngine>
 
     */
 
+    //TODO Ensure All states are added to statetable otherwise exit will crash
+
     /// <summary>
     /// Exit a state in a state machine
     /// </summary>
     /// <param name="ID">ID of State Machine</param>
     /// <param name="StateName">Name of the state to close</param>
     /// <param name="Immediately">Close state without ruinning it out componate </param>
-    public void ExtiState(Guid ID,string StateName,bool Immediately)
+    public void ExitState(Guid ID, string StateName, bool Immediately)
     {
-        StateMachine Machine = FindMachine(ID);
+        MonoStateMachine Machine = FindMachine(ID);
 
         State TargetState = (from Target in Machine.StateTable
                              where Target.Name == StateName
                              select Target).Single();
 
-        if(!Immediately)
+        if (!Immediately)
         {
             TargetState.Out();
-        } 
+        }
         Machine.ActiveStates.Remove(TargetState); // Remove State from State Stack
     }
 
@@ -191,9 +254,9 @@ public class GenericStateEngine : MonoService<GenericStateEngine>
     /// <param name="ID">ID of State Machine</param>
     /// <param name="Immediately">Close state without ruinning it out componate </param>
     /// <param name="StateName">Name of the state to close</param>
-    public void ExtiState(Guid ID, bool Immediately, params string[] StateName)
+    public void ExitState(Guid ID, bool Immediately, params string[] StateName)
     {
-        StateMachine Machine = FindMachine(ID);
+        MonoStateMachine Machine = FindMachine(ID);
 
         var TargetState = from Target in Machine.StateTable
                           where StateName.Contains(Target.Name)
@@ -215,50 +278,83 @@ public class GenericStateEngine : MonoService<GenericStateEngine>
     /// </summary>
     /// <param name="ID">ID of the State Machine, Each state machine has an ID </param>
     /// <returns></returns>
-    public StateMachine FindMachine(Guid ID)
+    public MonoStateMachine FindMachine(Guid ID)
     {
         var Machine = (from loadedMachine in LoadedMachines
                        where loadedMachine.MachineId == ID
                        select loadedMachine).Single();
 
 
-        if(Machine != null)
+        if (Machine != null)
         {
             return Machine;
         }
         else
         {
+            Debug.Log(string.Format("Unable to find State Machine {0}", ID));
             return null;
         }
 
     }
 
+    /// <summary>
+    /// Find a State machine using it User set Name
+    /// </summary>
+    /// <param name="ID">ID of the State Machine, Each state machine has an Name </param>
+    /// <returns></returns>
+    public MonoStateMachine FindMachine(string ID)
+    {
+        var Machine = (from loadedMachine in LoadedMachines
+                       where loadedMachine.Name == ID
+                       select loadedMachine).Single();
+
+
+        if (Machine != null)
+        {
+            return Machine;
+        }
+        else
+        {
+            Debug.Log(string.Format("Unable to find State Machine {0}", ID));
+            return null;
+        }
+
+    }
+    #endregion
 }
 
-#region StateMachineTemplateClasses
+#region StateMachine Template Classes
 /// <summary>
 /// Template State
 /// </summary>
 public abstract class State
 {
-    public List<State> NextStates = new List<State>();
-    
+    public State NextState
+    {
+        get { return m_NextStates[0]; }
+        set { m_NextStates.Insert(0, value); }
+    }
+    public List<State> NextStates
+    {
+        get { return m_NextStates; }
+        set { m_NextStates = value; }
+    }
     public State PreviousState
     {
         get { return m_PreviouseState[0]; }
-        set { m_PreviouseState.Insert(0,value); }
+        set { m_PreviouseState.Insert(0, value); }
     }
-
     public List<State> PreviousStates
     {
         get { return m_PreviouseState; }
         //set { m_PreviouseState = value; }
     }
 
+    private List<State> m_NextStates = new List<State>();
     //There may be a case where you want to have multiply previouse states but functionaily you only neeed one, this is refect in it property
-    public List<State> m_PreviouseState = new List<State>();
+    private List<State> m_PreviouseState = new List<State>();
 
-    public string Name;
+    public string Name = "NotNamed";
 
     public abstract void In();
 
@@ -266,20 +362,36 @@ public abstract class State
 
     public abstract void Out();
 
+    public virtual void Reset() { }
+
 }
 
 //Maybe Rethink having the statemachine as clases and insted impliment them via an inferface?
 /// <summary>
 /// Base State Machine with monobehaviour
 /// </summary>
-public class StateMachine :MonoBehaviour
+public abstract class MonoStateMachine : MonoBehaviour
 {
-
     /// <summary>
     /// Identifier for the machine
     /// </summary>
     public Guid MachineId = Guid.NewGuid();
 
+    [SerializeField]
+    /// <summary>
+    /// User Set Name of the State Machine for identfiction
+    /// </summary>
+    public string Name
+    {
+        get { return Name; }
+        set { SetName(value); }//Should be using list function to modify list
+
+    }
+
+    /// <summary>
+    /// User set Name for the server
+    /// </summary>
+    private string m_Name;
     /// <summary>
     /// Get the Active State of the state machine
     /// </summary>
@@ -309,25 +421,212 @@ public class StateMachine :MonoBehaviour
         //set { M_ActiveStates = value; }//Should be using list function to modify list
     }
 
-
-
     public List<State> M_ActiveStates = new List<State>();
     public List<State> StateTable = new List<State>();
+
+    /// <summary>
+    /// The Interal State, Disabled = not running
+    /// </summary>
     public StateMachinceStates InteralState = StateMachinceStates.Disabled;
+
+    /// <summary>
+    /// Search, StateTable by Name
+    /// </summary>
+    /// <param name="Name"></param>
+    /// <returns></returns>
+    public bool SearchStateTable(string Name)
+    {
+        for (int i = 0; i < StateTable.Count; i++)
+        {
+            if (Name.Equals(StateTable[i].Name))
+                return true;
+        }
+        return false;
+    }
+    /// <summary>
+    /// Search, StateTable by type
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    public bool SearchStateTable(Type type)
+    {
+        for (int i = 0; i < StateTable.Count; i++)
+        {
+            if (StateTable[i].GetType().Equals(type))
+                return true;
+        }
+        return false;
+    }
+    /// <summary>
+    /// Add a state to the State Machine, adds to active state and StateTable
+    /// </summary>
+    /// <param name="state">State to add to state machine </param>
+    public void AddState(State state)
+    {
+        M_ActiveStates.Add(state);
+        StateTable.Add(state);
+    }
+    /// <summary>
+    /// Add state to the State Table only.
+    /// </summary>
+    /// <param name="state">State to add to table</param>
+    public void AddToStateTable(State state)
+    {
+        StateTable.Add(state);
+    }
+    private void SetName(string Name)
+    {
+        var GSM = Services.Resolve<GenericStateEngine>();
+        
+        foreach (var SM in GSM.LoadedMachines)
+        {
+            if(SM.Name.Equals(Name))
+            {
+                m_Name = string.Format("StateMachine_{0}", Time.realtimeSinceStartup);
+                Debug.Log(string.Format("Name '{0}' Taken, Assigned name {1)",Name,m_Name));
+
+                return;
+            }
+        }
+
+        m_Name = Name;
+    }
+    public abstract void BuildStateTable();
 
 }
 
-/* State Machine with a scriptableobject, anti pattern state machine need a singular base to exentd from for the GSE
+///State Machine with a scriptableobject, anti pattern state machine need a singular base to exentd from for the GSE
 /// <summary>
 /// Base State Machine with SriptableObject
 /// </summary>
-public class ScriptableStateMachine : ScriptableObject
+public abstract class ScriptableStateMachine 
 {
+    /// <summary>
+    /// Link to engine
+    /// </summary>
+    GenericStateEngine m_Engine;
+
+    /// <summary>
+    /// Identifier for the machine
+    /// </summary>
     public Guid MachineId = Guid.NewGuid();
-    public List<State> ActiveState;
+
+    [SerializeField]
+    /// <summary>
+    /// User Set Name of the State Machine for identfiction
+    /// </summary>
+    public string Name
+    {
+        get { return Name; }
+        set { SetName(value); }//Should be using list function to modify list
+
+    }
+
+    /// <summary>
+    /// User set Name for the server
+    /// </summary>
+    private string m_Name;
+    /// <summary>
+    /// Get the Active State of the state machine
+    /// </summary>
+    public State ActiveState
+    {
+        get { return M_ActiveStates[0]; }
+        set
+        {
+            if (M_ActiveStates.Count > 0)
+            {
+                M_ActiveStates[0] = value;
+            }
+            else
+            {
+                M_ActiveStates.Add(value);
+            }
+
+        }
+    }
+
+    /// <summary>
+    /// Get the Active States of the StateMachine
+    /// </summary>
+    public List<State> ActiveStates
+    {
+        get { return M_ActiveStates; }
+        //set { M_ActiveStates = value; }//Should be using list function to modify list
+    }
+
+    public List<State> M_ActiveStates = new List<State>();
     public List<State> StateTable = new List<State>();
+
+    /// <summary>
+    /// The Interal State, Disabled = not running
+    /// </summary>
     public StateMachinceStates InteralState = StateMachinceStates.Disabled;
+
+    /// <summary>
+    /// Search, StateTable by Name
+    /// </summary>
+    /// <param name="Name"></param>
+    /// <returns></returns>
+    public bool SearchStateTable(string Name)
+    {
+        for (int i = 0; i < StateTable.Count; i++)
+        {
+            if (Name.Equals(StateTable[i].Name))
+                return true;
+        }
+        return false;
+    }
+    /// <summary>
+    /// Search, StateTable by type
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    public bool SearchStateTable(Type type)
+    {
+        for (int i = 0; i < StateTable.Count; i++)
+        {
+            if (StateTable[i].GetType().Equals(type))
+                return true;
+        }
+        return false;
+    }
+    /// <summary>
+    /// Add a state to the State Machine, adds to active state and StateTable
+    /// </summary>
+    /// <param name="state">State to add to state machine </param>
+    public void AddState(State state)
+    {
+        M_ActiveStates.Add(state);
+        StateTable.Add(state);
+    }
+    /// <summary>
+    /// Add state to the State Table only.
+    /// </summary>
+    /// <param name="state">State to add to table</param>
+    public void AddToStateTable(State state)
+    {
+        StateTable.Add(state);
+    }
+    private void SetName(string Name)
+    {
+        var GSM = Services.Resolve<GenericStateEngine>();
+
+        foreach (var SM in GSM.LoadedMachines)
+        {
+            if (SM.Name.Equals(Name))
+            {
+                m_Name = string.Format("StateMachine_{0}", Time.realtimeSinceStartup);
+                Debug.Log(string.Format("Name '{0}' Taken, Assigned name {1)", Name, m_Name));
+
+                return;
+            }
+        }
+
+        m_Name = Name;
+    }
+    public abstract void BuildStateTable();
 }
-*/
+
 
 #endregion
--- Assets/Scripts/GenericStateEngine/TestMachine.cs
+++ Assets/Scripts/GenericStateEngine/TestMachine.cs
@@ -6,39 +6,48 @@ using System.Collections.Generic;
 using System.Text;
 
 //Oliver.T 06-06-2024
-public class TestMachine : StateMachine
+public class TestMachine : MonoStateMachine
 {
 
     public List<GameObject> Cubes = new List<GameObject>();
 
-
     private void Start()
-    {
+    { 
         BuildStateTable();
-        Services.Resolve<GenericStateEngine>().StartMachine(this);
+        Services.Resolve<GenericStateEngine>().LoadStateMachine(this);
     }
-
 
     /// <summary>
     /// Construct the interals of the state machine
     /// </summary>
-    void BuildStateTable()
+    public override void BuildStateTable()
     {
         var BeginState = new Begin(this,"Begin");
+        var Timer = new UtilityStates.Timer<TestMachine>(this, UnityEngine.Random.Range(1, 20), "Timer_1");
         var MiddleState = new Middle(this,"Middle");
         var EndState = new End(this,"End");
 
         BeginState.NextStates.Add(MiddleState);
+
+        Timer.TimerFinshed += () =>
+        {         
+            foreach (var Target in Cubes)
+            {
+                Target.GetComponent<Renderer>().material.color = Color.red;
+            }
+        };
+
         MiddleState.NextStates.Add(EndState);
         EndState.NextStates.Add(BeginState);
 
         ActiveState = BeginState; //could also be ActiveState.Add(BeginState)
+        ActiveStates.Add(Timer);
 
+        StateTable.Add(Timer);
         StateTable.Add(BeginState);
         StateTable.Add(MiddleState);
         StateTable.Add(EndState);
     }
-
 
     internal class Begin : State
     {
@@ -58,7 +67,7 @@ public class TestMachine : StateMachine
         {
             Cube.GetComponent<Renderer>().material.color = Color.red;
             //Debug.Log(string.Format("Entering {0} in FSM {1}", MethodInfo.GetCurrentMethod(), machine.MachineId));
-            Debug.Log(string.Format("My Type is {0}",GetType().ToString()));
+            //Debug.Log(string.Format("My Type is {0}",GetType().ToString()));
         }
 
         public override void Out()
@@ -75,8 +84,8 @@ public class TestMachine : StateMachine
             if (Count > 1000)
             {
                 Count = 0;             
-                Services.Resolve<GenericStateEngine>().ChangeState(machine.MachineId,StateChange.Next);
-                //Services.Resolve<GenericStateEngine>().ChangeState(machine.MachineId,StateChange.Jump,"End");
+                //Services.Resolve<GenericStateEngine>().ChangeState(machine.MachineId,StateChange.Next,Name);
+                Services.Resolve<GenericStateEngine>().ChangeState(machine.MachineId,"End",Name);
             }
         }
     }
@@ -98,7 +107,7 @@ public class TestMachine : StateMachine
             Cube.GetComponent<Renderer>().material.color = Color.red;
             //machine.ActiveState = this;
             //Debug.Log(string.Format("Entering {0} in FSM {1}", MethodInfo.GetCurrentMethod(), machine.MachineId));
-            Debug.Log(string.Format("My Type is {0}", GetType().ToString()));
+           //Debug.Log(string.Format("My Type is {0}", GetType().ToString()));
         }
 
         public override void Out()
@@ -114,7 +123,7 @@ public class TestMachine : StateMachine
             if (Count > 1000)
             {
                 Count = 0;
-                Services.Resolve<GenericStateEngine>().ChangeState(machine.MachineId, StateChange.Next);
+                Services.Resolve<GenericStateEngine>().ChangeState(machine.MachineId, StateChange.Next, Name);
             }
         }
     }
@@ -135,15 +144,26 @@ public class TestMachine : StateMachine
         public override void In()
         {
             Cube.GetComponent<Renderer>().material.color = Color.red;
-            //machine.ActiveState = this;
-            //Debug.Log(string.Format("Entering {0} in FSM {1}", MethodInfo.GetCurrentMethod(), machine.MachineId));
-            Debug.Log(string.Format("My Type is {0}", GetType().ToString()));
         }
 
         public override void Out()
         {
+            if (machine.SearchStateTable("TimerEnd"))
+            {
+                var NewTimer = new UtilityStates.Timer<MonoStateMachine>(machine,UnityEngine.Random.Range(1,20), "TimerEnd");
+                NewTimer.PreviousState = this;
+                NewTimer.TimerFinshed += () =>
+                {
+                    foreach(var Targets in machine.Cubes)
+                    {
+                        Targets.GetComponent<Renderer>().material.color = Color.green;
+                    }
+                    
+                };
+                machine.AddState(NewTimer);
+            }
+
             Cube.GetComponent<Renderer>().material.color = Color.gray;
-            //Debug.Log(string.Format("Leaveing {0} in FSM {1}",MethodInfo.GetCurrentMethod(),machine.MachineId));
         }
 
         public override void Tick()
@@ -153,9 +173,14 @@ public class TestMachine : StateMachine
             if (Count > 1000)
             {
                 Count = 0;
-                Services.Resolve<GenericStateEngine>().ChangeState(machine.MachineId, StateChange.Next);
-                //Services.Resolve<GenericStateEngine>().ChangeState(machine.MachineId, StateChange.Jump, "Begin");
+                Services.Resolve<GenericStateEngine>().ChangeState(machine.MachineId, StateChange.Next,Name);
             }
+        }
+
+        public override void Reset()
+        {
+            base.Reset();
+            Count = 0;
         }
     }
 
